cae1239a59bbb45c7b09ac28c7c58e396367d073
Akash <akashmagar7500@gmail.com>
2025-08-22 23:01:52 +0530

    learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 const App = () => {
   return (
     <div className="w-full h-screen bg-black p-1 text-white">
-      <motion.div
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        transition={{ duration: 0.5, ease: "linear" }}
-        className="w-40 h-40 bg-emerald-500 m-4"
-      />
+      <AnimatePresence>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1, skewX: 10, rotate: [10, 20, 30, 50, 0] }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, ease: "linear" }}
+          className="w-40 h-40 bg-emerald-500 m-4"
+        />
+      </AnimatePresence>
+
       <motion.button
         whileHover={{ scale: 1.1 }}
-        whileTap={{ backgroundColor: "rgb(255, 0, 0)" }}
+        whileTap={{ backgroundColor: "rgb(255, 0, 0)", originX: 50 }}
         onHoverStart={() => console.log("hover started!")}
       >
         Button
